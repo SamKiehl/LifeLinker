44505523bb7ba3e7d42d992a20d33b5ed05a005c
Sam Kiehl <e22-kiehlsg@elderhs.net>
2023-06-15 18:38:21 -0400

    Fixed toggle button functionality.

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
 var noSleepOn = false;
 
-if ('wakeLock' in navigator) {
-    let wakeLock = null;
-  
-    // Function to request a wake lock
-    async function requestWakeLock() {
-      try {
-        wakeLock = await navigator.wakeLock.request('screen');
-        console.log('Screen wake lock engaged!');
-        alert('Screen wake lock engaged!');
-      } catch (error) {
-        console.error(`${error.name}, ${error.message}`);
-      }
-    }
-  
-    // Function to release the wake lock
-    function releaseWakeLock() {
-      if (wakeLock !== null) {
-        wakeLock.release().then(() => {
-          console.log('Screen wake lock released.');
-          alert('Screen wake lock released');
-        });
-        wakeLock = null;
-      }
-    }
+let wakeLock = null;
+
+// Function to request a wake lock
+async function requestWakeLock() {
+  try {
+    wakeLock = await navigator.wakeLock.request('screen');
+    console.log('Screen wake lock engaged!');
+  } catch (error) {
+    console.error(`${error.name}, ${error.message}`);
+  }
+}
+
+// Function to release the wake lock
+function releaseWakeLock() {
+  if (wakeLock !== null) {
+    wakeLock.release().then(() => {
+      console.log('Screen wake lock released.');
+    });
+    wakeLock = null;
+  }
 }
 
 var playerDiv = document.getElementById("Players");
@@ -37,14 +33,16 @@ var playerObjects = [];
 
 var keepScreenAwake = document.getElementById("NoSleep");
 
-keepScreenAwake.addEventListener("click", () => {
+keepScreenAwake.addEventListener("click", async () => {
     if(noSleepOn){
         releaseWakeLock();
         alert("Screen Lock Disabled");
+        noSleepOn = false;
         return;
-    }
+      }
     requestWakeLock();
     alert("Screen Lock Enabled");
+    noSleepOn = true;
 });
 
 var addPlayer = document.getElementById("AddPlayer");
@@ -60,9 +58,9 @@ addPlayer.addEventListener("click", () => {
     
     playerDiv.innerHTML = players;
     
-});
-
-generateNewPlayer = (startingLife, numPlayers) => {
+  });
+  
+  generateNewPlayer = (startingLife, numPlayers) => {
     const number = numPlayers;
     x = {};
     x.name = `Player ${number}`;
@@ -71,29 +69,29 @@ generateNewPlayer = (startingLife, numPlayers) => {
     x.addLife = (amount) => {x.life += amount};
     x.html = `<div class="player">${x.name}<br><div id="Life"><b>${x.life}</b></div><br><button onclick="addLife(${x.index}, -10);", id="MinusMinus"><b> -10 </b></button><button onclick="addLife(${x.index}, -1);", id="Minus"><b> -1 </b></button>  <button onclick="addLife(${x.index}, 1);", id="Plus"><b> +1 </b></button><button onclick="addLife(${x.index}, 10);", id="PlusPlus"><b> +10 </b></button></div>`;
     x.updateHTML = () => x.html = `<div class="player">${x.name}<br><div id="Life"><b>${x.life}</b></div><br><button onclick="addLife(${x.index}, -10);", id="MinusMinus"><b> -10 </b></button><button onclick="addLife(${x.index}, -1);", id="Minus"><b> -1 </b></button>  <button onclick="addLife(${x.index}, 1);", id="Plus"><b> +1 </b></button><button onclick="addLife(${x.index}, 10);", id="PlusPlus"><b> +10 </b></button></div>`;
-
+    
     //console.log(x);
     return x;
-}
-
-addLife = (playerNumber, amount) => {
+  }
+  
+  addLife = (playerNumber, amount) => {
     //console.log(`PlayerNumber: ${playerNumber}`)
     x = playerObjects[playerNumber];
     
 
     x.addLife(amount);
     refreshPlayerObjects();
-}
-
-refreshPlayerObjects = () => {
+  }
+  
+  refreshPlayerObjects = () => {
     players = ``;
     for(let y in playerObjects){
-        playerObjects[y].updateHTML();
-        players += playerObjects[y].html;
-
-        console.log(y)
+      playerObjects[y].updateHTML();
+      players += playerObjects[y].html;
+      
+      console.log(y)
         players += ((y % 2 == 1)) ? '<br><br>':'';
     }
     playerDiv.innerHTML = players;
 }
-
+  
